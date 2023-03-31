a8c7593114264bc969c14d7aa49958b7a9ecc580
Kainy <iartkhay@gmail.com>
2023-03-31 02:16:55 +0100

    final font fixes

--- src/components/Headings.js
+++ src/components/Headings.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const H2 = ({ children, className, ...props }) => {
   return (
-    <h2 className={"text-off-black font-semibold text-3xl  font-droid " + className} {...props}>
+    <h2 className={"text-off-black font-semibold text-3xl leading-[40px] font-droid " + className} {...props}>
       {children}
     </h2>
   )
@@ -10,7 +10,7 @@ const H2 = ({ children, className, ...props }) => {
 
 const H5 = ({ children, className, ...props }) => {
   return (
-    <h5 className={"text-black text-base font-light tracking-wider mt-2 font-montserrat" + className} {...props}>
+    <h5 className={"text-black text-base font-light tracking-wide leading-[30px] mt-[8px] font-montserrat" + className} {...props}>
       {children}
     </h5>
   )
