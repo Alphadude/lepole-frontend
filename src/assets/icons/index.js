dae66653a8344537795787aefd47b3a7e2f21858
droxy <chiomaotu22@gmail.com>
2023-04-04 06:20:32 +0100

    added layout icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,4 +1,14 @@
 import LePoleLogo from './lePole-logo.svg';
 import BackArrow from './back-arrow.svg';
-
-export { LePoleLogo, BackArrow };
+import DropdownIcon from './dropdown.svg';
+import NotificationIcon2 from './notification2.svg';
+import LePoleLogoBlack from './le-pole-black.svg';
+import LogoutIcon from './logout.svg';
+export {
+  LePoleLogo,
+  BackArrow,
+  DropdownIcon,
+  NotificationIcon2,
+  LePoleLogoBlack,
+  LogoutIcon,
+};
