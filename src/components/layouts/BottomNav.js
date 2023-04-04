6104945987c874a6468de6e411ea77236cdc2ce5
droxy <chiomaotu22@gmail.com>
2023-04-04 06:21:16 +0100

    created bottom nav component

---
+++ src/components/layouts/BottomNav.js
@@ -0,0 +1,36 @@
+import { Link, useLocation } from 'react-router-dom';
+
+import { sidebarItems } from './SidebarItem';
+
+const BottomNav = () => {
+  const { pathname } = useLocation();
+
+  return (
+    <nav className="lg:hidden shadow-4xl fixed left-0 right-0 bottom-0 w-full bg-white">
+      <ul className="py-6 px-4 flex items-center justify-between">
+        {sidebarItems.map(({ Icon, ...item }) => (
+          <Link
+            key={item.title}
+            to={item.link}
+            className={`rounded-full p-3
+             cursor-pointer duration-100 text-sm font-medium ${
+               pathname === item.link
+                 ? 'bg-primary-green rounded-full'
+                 : 'hover:bg-primary-white '
+             }`}
+          >
+            <div>
+              <Icon
+                height="20px"
+                width="20px"
+                stroke={pathname === item.link ? 'white' : '#73797F'}
+              />
+            </div>
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default BottomNav;
