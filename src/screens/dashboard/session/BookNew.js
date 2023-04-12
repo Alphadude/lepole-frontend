ccb35e74e99f30a33c710b3dd2487c92db87a0e5
Kainy <iartkhay@gmail.com>
2023-04-12 16:58:53 +0100

    fixed responsive issues

--- src/screens/dashboard/session/BookNew.js
+++ src/screens/dashboard/session/BookNew.js
@@ -44,28 +44,28 @@ const PlanCard = ({ id, name, desc, startTime, endTime, fiat_price, coin_price, 
   return (
     <div
       role='button'
-      className={` border border-gray-4 rounded-2xl h-36 w-80 md:h-[174px] md:w-[446px] p-4 md:p-6 gap-x-3 space-y-4 md:space-y-8 text-renaissance-black dark:text-renaissance-dark-black  
+      className={` border border-gray-4 rounded-2xl h-36w-80 lg:h-[174px] lg:w-[446px] p-4 lg:p-6 gap-x-3 space-y-4 lg:space-y-8 text-renaissance-black dark:text-renaissance-dark-black  
       ${selected === id && ' bg-primary-green/30'}`}
       onClick={() => setSelected(id)}
     >
       <section className={`flex`} >
-        <div className='flex-1 space-y-1 md:space-y-4'>
-          <h5 className='text-sm md:text-lg font-semibold'> {name} </h5>
-          <p className='text-primary-gray font-normal text-xs md:text-sm'> {desc} </p>
+        <div className='flex-1 space-y-1 lg:space-y-4'>
+          <h5 className='text-sm lg:text-lg font-semibold'> {name} </h5>
+          <p className='text-primary-gray font-normal text-xs lg:text-sm'> {desc} </p>
         </div>
         <div>
           <p className='flex items-baseline text-primary-gray'>
-            <span className='self-start pr-0.5 text-xs md:text-base'>£</span>
-            <span className='font-bold font-droid text-[32px] md:text-5xl text-renaissance-black dark:text-renaissance-dark-black'> {fiat_price} </span>
+            <span className='self-start pr-0.5 text-xs lg:text-base'>£</span>
+            <span className='font-bold font-droid text-[32px] lg:text-5xl text-renaissance-black dark:text-renaissance-dark-black'> {fiat_price} </span>
             /hour
           </p>
         </div>
       </section>
 
       <section className='flex items-baseline justify-between '>
-        <div className='flex-1 flex items-baseline  gap-x-1 md:gap-x-8'>
+        <div className='flex-1 flex items-baseline  gap-x-1 lg:gap-x-8'>
           <div className='bg-badge-gray font-normal py-0.5 px-2 text-xxs rounded-xlg text-neutral-700'>Available Time</div>
-          <p className='font-semibold text-xxs md:text-xs'>
+          <p className='font-semibold text-xxs lg:text-xs'>
             {startTime}:00 AM - {endTime}:00 PM
           </p>
         </div>
@@ -89,7 +89,7 @@ const BookNew = () => {
 
 
   return (
-    <div className=' text-renaissance-black dark:text-renaissance-dark-black pl-[4%] pr-[8%] pt-6 pb-24 transition  '>
+    <div className=' text-renaissance-black dark:text-renaissance-dark-black p-[4%] xl:pr-[8%] pt-6 pb-24 transition w-full '>
       <section className=''>
         <H3> Book a session </H3>
         <P className='  '>
@@ -104,7 +104,7 @@ const BookNew = () => {
       </section>
 
       <section className='flex lg:flex-row flex-col items-center lg:items-start gap-y-10 lg:gap-y-0 justify-between'>
-        <div className='max-w-min text-center lg:text-left'>
+        <div className=' lg:pr-[%] xl:pr-[20%] text-center lg:text-left'>
           <div>
             <H3>Select Date and Time</H3>
             <P className='pt-2 pb-10'>In your local time GMT +8 <span className='text-renaissance-blue pl-2'>Update </span></P>
@@ -129,7 +129,7 @@ const BookNew = () => {
         </div>
 
 
-        <div className=''>
+        <div className='w-full max-w-2xl'>
           <H3>Pay-As-You-Go</H3>
           <P className='pt-2 pb-10'>In your local time GMT +8 <span className='text-renaissance-blue pl-2'>Update </span></P>
 
