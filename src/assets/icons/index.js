418c6998b1a22d927ee1eba58ae63bdc73463983
droxy <chiomaotu22@gmail.com>
2023-04-12 06:09:17 +0100

    added more icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,12 +1,17 @@
 import LePoleLogo from './lePole-logo.svg';
 import BackArrow from './back-arrow.svg';
 import DropdownIcon from './dropdown.svg';
+import CalendarIcon from './calendar-black.svg';
+import NotificationIcon from './notification-black.svg';
 import NotificationIcon2 from './notification2.svg';
 import LePoleLogoBlack from './le-pole-black.svg';
 import LogoutIcon from './logout.svg';
 import DumbellOrange from './dumbell-orange.svg';
 import CoinGreen from './coin-green.svg';
 import MoneyBlue from './money-blue.svg';
+import GoldCoins from './coins-gold.svg';
+import CaretRightWhite from './caret-right-white.svg';
+
 export {
   LePoleLogo,
   BackArrow,
@@ -17,4 +22,8 @@ export {
   DumbellOrange,
   CoinGreen,
   MoneyBlue,
+  NotificationIcon,
+  CalendarIcon,
+  GoldCoins,
+  CaretRightWhite,
 };
