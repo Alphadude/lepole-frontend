3d85966cd7f899751174c891286a0d7427e2436c
droxy <chiomaotu22@gmail.com>
2023-04-12 06:39:37 +0100

    overview card made completely responsive

--- src/components/sections/explore/OverviewCard.js
+++ src/components/sections/explore/OverviewCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const OverviewCard = ({ title, icon, figures, bgColor, textColor }) => {
   return (
-    <div className="bg-white shadow-6xl flex items-start rounded-lg h-20 !px-4 pt-4">
+    <div className="md:h-20 bg-white shadow-6xl flex items-start rounded-lg  !px-2 lg:!px-4 py-4">
       <div
         className={`${bgColor} h-4 w-4 rounded-full flex items-center justify-center`}
       >
@@ -10,11 +10,11 @@ const OverviewCard = ({ title, icon, figures, bgColor, textColor }) => {
       </div>
 
       <div className="ml-2 flex-1">
-        <div className="capitalize text-primary-gray text-[10px] sm:text-xs font-medium">
+        <div className="capitalize text-primary-gray text-xs font-medium">
           {title}
         </div>
 
-        <div className={`${textColor} font-bold text-sm lg:text-xl`}>
+        <div className={`${textColor} font-bold text-lg md:text-xl`}>
           {figures}
         </div>
       </div>
