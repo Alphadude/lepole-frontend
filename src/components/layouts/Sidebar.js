20306b69e7a8c55fc8e3a9e7961e6a704486271d
droxy <chiomaotu22@gmail.com>
2023-04-15 12:20:13 +0100

    cleaned up styling

--- src/components/layouts/Sidebar.js
+++ src/components/layouts/Sidebar.js
@@ -2,25 +2,37 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 import { sidebarItems } from './SidebarItem';
 
-import { LePoleLogoBlack, LePoleLogoWhite, LogoutIcon } from '../../assets/icons';
+import {
+  LePoleLogoBlack,
+  LePoleLogoWhite,
+  LogoutIcon,
+} from '../../assets/icons';
 import { useCookies } from 'react-cookie';
 
 const Sidebar = () => {
   const { pathname } = useLocation();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [cookies, setCookie, removeCookie] = useCookies(['user']);
 
   const handleLogout = () => {
-    removeCookie();
+    removeCookie('user', { path: null });
     navigate('/');
-  }
+  };
 
   return (
     <div className="hidden lg:flex shadow-3xl flex-col w-60 h-full pb-8 bg-white text-gray-5 dark:bg-dark-white ">
       <div className="px-6">
         <div className="text-center flex items-center justify-center ">
-          <img src={LePoleLogoBlack} alt="le pole logo" className='inline dark:hidden' />
-          <img src={LePoleLogoWhite} alt="le pole logo" className='hidden dark:inline' />
+          <img
+            src={LePoleLogoBlack}
+            alt="le pole logo"
+            className="inline dark:hidden"
+          />
+          <img
+            src={LePoleLogoWhite}
+            alt="le pole logo"
+            className="hidden dark:inline"
+          />
         </div>
       </div>
 
@@ -30,20 +42,27 @@ const Sidebar = () => {
             key={item.title}
             to={item.link}
             className={`flex items-center rounded-lg py-2.5 pl-5
-             cursor-pointer duration-100 text-sm font-medium mt-3 dark:text-gray-4 ${pathname.includes(item.link)
-                ? 'bg-primary-green text-white'
-                : 'text-gray-5 hover:bg-gray-4/40  '
-              }`}
+             cursor-pointer duration-100 text-sm font-medium mt-3 dark:text-gray-4 ${
+               pathname.includes(item.link)
+                 ? 'bg-primary-green text-white'
+                 : 'text-gray-5 hover:bg-gray-4/40 hover:dark:bg-primary-green/20'
+             }`}
           >
             <div>
-              <Icon stroke={pathname.includes(item.link) ? 'white' : '#73797F'} />
+              <Icon
+                stroke={pathname.includes(item.link) ? 'white' : '#73797F'}
+              />
             </div>
             <span className="capitalize block ml-2">{item.title}</span>
           </Link>
         ))}
       </ul>
 
-      <div role='button' onClick={handleLogout} className="w-fit mx-auto mt-auto flex items-center cursor-pointer ">
+      <div
+        role="button"
+        onClick={handleLogout}
+        className="w-fit mx-auto mt-auto flex items-center cursor-pointer "
+      >
         <div>
           <img src={LogoutIcon} alt="log out" />
         </div>
