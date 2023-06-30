2d80347ac807d8eba35ebd9fcd20dd902e3420bc
Kainy <iartkhay@gmail.com>
2023-06-30 10:27:19 +0100

    add new design for plan cards

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 import gymCharacters from './gym-characters.png';
 import LepoleLogoHalf from './lePole_half.svg';
+import coinsBookSession from './coins_book_session.png'
 
-export { gymCharacters, LepoleLogoHalf };
+export { gymCharacters, LepoleLogoHalf, coinsBookSession };
