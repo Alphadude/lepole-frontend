f7ff47260307fed85f1b83f6e9eea266a3306cd3
droxy <chiomaotu22@gmail.com>
2023-12-07 19:33:55 +0100

    added cancel modal

--- src/screens/dashboard/session/Upcoming.js
+++ src/screens/dashboard/session/Upcoming.js
@@ -1,54 +1,55 @@
-import React, { useState } from 'react'
-import SessionsLayout from '../../../components/layouts/SessionsLayout'
+import React, { useState } from 'react';
+import SessionsLayout from '../../../components/layouts/SessionsLayout';
 import { createColumnHelper } from '@tanstack/react-table';
 import Tables from '../../../components/Tables';
 import ModalContainer from '../../../components/layouts/ModalContainer';
-import { RescheduleModal } from '../../../components/Modals';
-import { useSessions, useUpcomingSessions } from '../../../helpers/hooks/queries/useSessions';
+import { RescheduleModal, CancelModal } from '../../../components/Modals';
+
+import {
+  useSessions,
+  useUpcomingSessions,
+} from '../../../helpers/hooks/queries/useSessions';
 // import Loader from '../../../components/Loader';
-import gymCouple from '../../../assets/images/gym_couple.png'
+import gymCouple from '../../../assets/images/gym_couple.png';
 import { Link } from 'react-router-dom';
 import { Button } from '@deposits/ui-kit-react';
 import { routes } from '../../../router/routes';
 import { plans } from '../../../utils/dummyData';
 import moment from 'moment';
 
-
 const upComingRows = [
   {
     amount: 6,
-    created_at: "2023-04-23T00:50:24.922936+00:00",
-    date: "2023-04-23",
-    duration: "3 hrs",
-    endTime: "2023-04-23T02:00:00+00:00",
+    created_at: '2023-04-23T00:50:24.922936+00:00',
+    date: '2023-04-23',
+    duration: '3 hrs',
+    endTime: '2023-04-23T02:00:00+00:00',
     id: 35,
-    paymentType: "coin balance",
-    startTime: "2023-04-22T23:00:00+00:00",
-    status: "pending",
-    type: "Off PeaK",
-    user_id: "c753c13c-4218-4e44-9420-7c90be48cf0d",
-  }
-]
-
+    paymentType: 'coin balance',
+    startTime: '2023-04-22T23:00:00+00:00',
+    status: 'pending',
+    type: 'Off PeaK',
+    user_id: 'c753c13c-4218-4e44-9420-7c90be48cf0d',
+  },
+];
 
 export const initialDataSessions = {
   id: '',
   planId: 0,
   data: {
     amount: 0,
-    created_at: "",
-    date: "",
-    duration: "",
-    endTime: "",
+    created_at: '',
+    date: '',
+    duration: '',
+    endTime: '',
     id: 0,
-    paymentType: "",
-    startTime: "",
-    status: "",
-    type: "",
-    user_id: "",
-  }
-}
-
+    paymentType: '',
+    startTime: '',
+    status: '',
+    type: '',
+    user_id: '',
+  },
+};
 
 const Upcoming = ({
   loading = false,
@@ -58,16 +59,21 @@ const Upcoming = ({
   totalPage,
   limit,
 }) => {
-  const [modalOpen, setModalOpen] = useState(false)
-  const [selectedSession, setSelectedSession] = useState(initialDataSessions)
+  const [modalOpen, setModalOpen] = useState(false);
+  const [openCancel, setOpenCancel] = useState(false);
+  const [selectedSession, setSelectedSession] = useState(initialDataSessions);
 
-  const { data, isLoading } = useUpcomingSessions()
-
+  const { data, isLoading } = useUpcomingSessions();
 
   const toggleModal = (session) => {
-    setModalOpen(prev => !prev)
-    session?.id && setSelectedSession(session)
-  }
+    setModalOpen((prev) => !prev);
+    session?.id && setSelectedSession(session);
+  };
+
+  const cancelToggle = (session) => {
+    setOpenCancel((prev) => !prev);
+    session?.id && setSelectedSession(session);
+  };
 
   const columnHelper = createColumnHelper();
 
@@ -92,7 +98,9 @@ const Upcoming = ({
 
         return (
           <div className="text-sm">
-            <span className=" text-sm text-priBlack">{moment(date).format('Do MMMM, YYYY')}</span>
+            <span className=" text-sm text-priBlack">
+              {moment(date).format('Do MMMM, YYYY')}
+            </span>
           </div>
         );
       },
@@ -105,16 +113,20 @@ const Upcoming = ({
 
         return (
           <div className="text-sm">
-            {value?.passcode
-              || <>
-                Unavailable
-                [<a href={`mailto:contact@lepoleltd.com?subject=Lepole%20Booking%20App%20Access%20Code%20Request`}
-                  target='_blank' rel='noreferrer' className=" text-sm text-primary-blue"
+            {value?.passcode || (
+              <>
+                Unavailable [
+                <a
+                  href={`mailto:contact@lepoleltd.com?subject=Lepole%20Booking%20App%20Access%20Code%20Request`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className=" text-sm text-primary-blue"
                 >
                   contact support
-                </a>]
+                </a>
+                ]
               </>
-            }
+            )}
           </div>
         );
       },
@@ -138,69 +150,39 @@ const Upcoming = ({
       ),
     }),
 
-
-    // columnHelper.accessor((row) => row.role, {
-    //   id: 'Start Time',
-    //   cell: (info) => (
-    //     <span className="text-priBlack text-sm font-semibold">
-    //       {info.getValue()}
-    //     </span>
-    //   ),
-    // }),
-
-    // columnHelper.accessor((row) => row.role, {
-    //   id: 'End Time',
-    //   cell: (info) => (
-    //     <span className="text-priBlac text-sm font-semibold">
-    //       {info.getValue()}
-    //     </span>
-    //   ),
-    // }),
-
-    // rows?.status
-    // && columnHelper.accessor((row) => 'status', {
-    //   id: 'Status',
-    //   cell: (info) => {
-    //     const { status } = info.row.original;
-    //     let statusStyle;
-
-    //     switch (status) {
-    //       case 'pending':
-    //         statusStyle = 'bg-[#FFF0DB] text-[#E08304]';
-    //         break;
-    //       case 'rejected':
-    //         statusStyle = 'bg-lightRed text-[#842432]';
-    //         break;
-    //       case 'accepted':
-    //         statusStyle = 'text-[#158957] bg-lightGreen';
-    //         break;
-    //       default:
-    //         statusStyle = 'bg-[#FFF0DB] text-[#E08304]';
-    //         break;
-    //     }
-    //     return (
-    //       <span
-    //         className={`flex justify-center py-[4px] ${statusStyle} rounded-2xl`}
-    //       >
-    //         {status}
-    //       </span>
-    //     );
-    //   },
-    // }),
-
     columnHelper.accessor(() => 'actions', {
       id: 'Actions',
       cell: (info) => {
-        const { row: { original } } = info;
+        const {
+          row: { original },
+        } = info;
 
         const setModalValues = () => {
-          toggleModal({ planId: original.planId, id: original.id, data: original })
-        }
+          toggleModal({
+            planId: original.planId,
+            id: original.id,
+            data: original,
+          });
+        };
+
+        const setCancelValues = () => {
+          cancelToggle({
+            planId: original.planId,
+            id: original.id,
+            data: original,
+          });
+        };
 
         return (
-          <button onClick={setModalValues} >
-            <p className='underline hover:no-underline'>Reschedule</p>
-          </button>
+          <div className="flex gap-4">
+            <button onClick={setModalValues}>
+              <p className="underline hover:no-underline">Reschedule</p>
+            </button>
+
+            <button onClick={setCancelValues}>
+              <p className="underline hover:no-underline">Cancel Booking</p>
+            </button>
+          </div>
         );
       },
     }),
@@ -209,65 +191,88 @@ const Upcoming = ({
   return (
     <SessionsLayout>
       {modalOpen && (
-        <ModalContainer scrollable modalOpen={modalOpen} toggleModal={toggleModal}>
-          <RescheduleModal toggleModal={toggleModal} setSelectedSession={setSelectedSession} selectedSession={selectedSession} planId={selectedSession?.planId} />
+        <ModalContainer
+          scrollable
+          modalOpen={modalOpen}
+          toggleModal={toggleModal}
+          back={true}
+        >
+          <RescheduleModal
+            toggleModal={toggleModal}
+            setSelectedSession={setSelectedSession}
+            selectedSession={selectedSession}
+            planId={selectedSession?.planId}
+          />
         </ModalContainer>
       )}
 
-      {isLoading
+      {openCancel && (
+        <ModalContainer
+          scrollable
+          modalOpen={cancelToggle}
+          toggleModal={cancelToggle}
+        >
+          <CancelModal
+            toggleModal={cancelToggle}
+            setSelectedSession={setSelectedSession}
+            selectedSession={selectedSession}
+            planId={selectedSession?.planId}
+          />
+        </ModalContainer>
+      )}
 
-        ? <div className=' text-center pt-52 '>
+      {isLoading ? (
+        <div className=" text-center pt-52 ">
           <p> loading... </p>
         </div>
-        : data?.data?.length === 0 ? (
-          <section className=' py-16 lg:py-12 qw-full h-full flex justify-center items-center'>
-            <div className='flex flex-col items-center'>
-              <img src={gymCouple} alt="empty state" className='w-3/5 lg:w-full' />
-              <p className='mt-10 mb-8'>
-                Oops! You do not have any upcoming session
-              </p>
-              <Link to={`/${routes.dashboard_home}/${routes.session}/${routes.new}`}>
-                <Button
-                  className="!bg-primary-green !w-full !border-0 !px-8 !text-primary-white"
-                  size="xlarge"
-                >
-                  Book Session
-                </Button>
-              </Link>
-            </div>
-          </section>
-        ) : (
-          <section className='text-xs mt-6 w-full  font-normal'>
-            <Link to={`/${routes.dashboard_home}/${routes.session}/${routes.new}`} className=' flex justify-end '>
+      ) : data?.data?.length === 0 ? (
+        <section className=" py-16 lg:py-12 qw-full h-full flex justify-center items-center">
+          <div className="flex flex-col items-center">
+            <img
+              src={gymCouple}
+              alt="empty state"
+              className="w-3/5 lg:w-full"
+            />
+            <p className="mt-10 mb-8">
+              Oops! You do not have any upcoming session
+            </p>
+            <Link
+              to={`/${routes.dashboard_home}/${routes.session}/${routes.new}`}
+            >
               <Button
-                className="!bg-primary-green border-0 !px-8 mb-6 !text-primary-white !hidden lg:!inline"
-                size="large"
+                className="!bg-primary-green !w-full !border-0 !px-8 !text-primary-white"
+                size="xlarge"
               >
-                Book New Session
-              </Button>
-              <Button
-                className="!bg-primary-green border-0 !px-4 mb-6 !text-primary-white lg:!hidden"
-                size="medium"
-              >
-                Book New Session
+                Book Session
               </Button>
             </Link>
+          </div>
+        </section>
+      ) : (
+        <section className="text-xs mt-6 w-full  font-normal">
+          <Link
+            to={`/${routes.dashboard_home}/${routes.session}/${routes.new}`}
+            className=" flex justify-end "
+          >
+            <Button
+              className="!bg-primary-green border-0 !px-8 mb-6 !text-primary-white !hidden lg:!inline"
+              size="large"
+            >
+              Book New Session
+            </Button>
+            <Button
+              className="!bg-primary-green border-0 !px-4 mb-6 !text-primary-white lg:!hidden"
+              size="medium"
+            >
+              Book New Session
+            </Button>
+          </Link>
 
-            <Tables
-              columns={columns}
-              data={data?.data}
-            />
-          </section>
-        )
-      }
-
-
+          <Tables columns={columns} data={data?.data} />
+        </section>
+      )}
     </SessionsLayout>
   );
 };
 
-
-
-
-
-export default Upcoming
+export default Upcoming;
