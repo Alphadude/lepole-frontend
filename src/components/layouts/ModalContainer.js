94232e647e574ef6ada87a8ab040e038a5be9bd9
droxy <chiomaotu22@gmail.com>
2023-12-07 19:33:44 +0100

    added cancel modal

--- src/components/layouts/ModalContainer.js
+++ src/components/layouts/ModalContainer.js
@@ -1,25 +1,46 @@
-import React from 'react'
+import React from 'react';
 
-const ModalContainer = ({ children, modalOpen, toggleModal, scrollable, zIndex, ...props }) => {
-
+const ModalContainer = ({
+  children,
+  modalOpen,
+  toggleModal,
+  scrollable,
+  zIndex,
+  back,
+  ...props
+}) => {
   return (
     <div
-      className={` top-0 left-0 absolute w-screen h-screen justify-center overflow-y-aut items-center bg-[#000000aa] dark:bg-[#000000cc] ${zIndex || 'z-20'} ${modalOpen ? 'flex flex-col opacity-100 transition-all delay-1000 ' : 'hidden opacity-0 delay-1000 transition-all'}  `}
-
+      className={` top-0 left-0 absolute w-screen h-screen justify-center overflow-y-aut items-center bg-[#000000aa] dark:bg-[#000000cc] ${
+        zIndex || 'z-20'
+      } ${
+        modalOpen
+          ? 'flex flex-col opacity-100 transition-all delay-1000 '
+          : 'hidden opacity-0 delay-1000 transition-all'
+      }  `}
       {...props}
     >
-      <div onClick={toggleModal} className={`w-full px-2 items-center  flex-1 overflow-y-auto border-red-500 flex flex-col justify- ${!scrollable && 'justify-center'} `}>
+      <div
+        onClick={toggleModal}
+        className={`w-full px-2 items-center  flex-1 overflow-y-auto border-red-500 flex flex-col justify- ${
+          !scrollable && 'justify-center'
+        } `}
+      >
         {children}
 
-        <div onClick={e => e.stopPropagation()} className='p-6 flex flex-col justify-center text-center '>
-          <button onClick={toggleModal} className='text-primary-white'>
-            Go back
-          </button>
-        </div>
+        {back && (
+          <div
+            onClick={(e) => e.stopPropagation()}
+            className="p-6 flex flex-col justify-center text-center "
+          >
+            <button onClick={toggleModal} className="text-primary-white">
+              Go back
+            </button>
+          </div>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default ModalContainer
+export default ModalContainer;
