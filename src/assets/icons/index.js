efde8a78b7a3e203af35800880e8399c2ff64012
droxy <chiomaotu22@gmail.com>
2023-12-07 19:32:39 +0100

    added icons

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -12,6 +12,8 @@ import CoinGreen from './coin-green.svg';
 import MoneyBlue from './money-blue.svg';
 import GoldCoins from './coins-gold.svg';
 import CaretRightWhite from './caret-right-white.svg';
+import WarningOrange from './warning-orange.svg';
+import RedCheck from './check-red.svg';
 
 export {
   LePoleLogo,
@@ -28,4 +30,6 @@ export {
   CalendarIcon,
   GoldCoins,
   CaretRightWhite,
+  WarningOrange,
+  RedCheck,
 };
