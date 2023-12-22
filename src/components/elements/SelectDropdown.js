eab50365c00464908f98e4e893ab8788fe4452ea
droxy <chiomaotu22@gmail.com>
2023-12-22 08:02:31 +0100

    made select dark mode theme

--- src/components/elements/SelectDropdown.js
+++ src/components/elements/SelectDropdown.js
@@ -5,9 +5,16 @@ const SelectDropdown = React.forwardRef(function SelectDropdownt(
   { defaultValue, onChange, styles, options, ...rest },
   ref,
 ) {
+  const darkMode =
+    localStorage.getItem('theme') ||
+    (window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light');
+
   const customStyles = {
     menu: (provided) => ({
       ...provided,
+      background: darkMode === 'dark' ? '#1E1E1E' : 'white',
       fontSize: '14px',
       textTransform: 'capitalize',
       zIndex: 9999999,
@@ -15,27 +22,27 @@ const SelectDropdown = React.forwardRef(function SelectDropdownt(
 
     placeholder: (provided) => ({
       ...provided,
-      color: '#006666',
+      color: darkMode === 'dark' ? 'white' : '#006666',
     }),
 
     control: (provided, state) => ({
       ...provided,
       fontSize: '14px',
       height: '50px',
-      background: 'white',
-      border: `1px solid ${
-        state.isFocused ? '#006666' : 'rgba(0, 102, 102, 0.5)'
-      }`,
+      background: darkMode === 'dark' ? '#1E1E1E' : 'white',
+
+      border: `1px solid ${state.isFocused ? '#006666' : 'white'}`,
       outline: 0,
       boxShadow: 0,
       WebkitBoxShadow: 0,
       MozBoxShadow: 0,
-      color: '#111',
+      color: 'red',
       textTransform: 'capitalize',
     }),
 
     option: (provided, state) => ({
       ...provided,
+
       backgroundColor: state.isSelected
         ? '#006666'
         : state.isFocused
@@ -44,10 +51,11 @@ const SelectDropdown = React.forwardRef(function SelectDropdownt(
     }),
 
     singleValue: (provided, state) => {
+      const color = darkMode === 'dark' ? '#fff' : '#111';
       const opacity = state.isDisabled ? 0.5 : 1;
       const transition = 'opacity 300ms';
 
-      return { ...provided, opacity, transition };
+      return { ...provided, opacity, transition, color };
     },
   };
 
